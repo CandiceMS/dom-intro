c4adbabe2a1a72aa9b965e959fe13ad4bce6c981
Candice <candice.m.sprout@gmail.com>
2018-05-14 17:40:04 +0200

    Text input merged

--- js/text-bill_dom.js
+++ js/text-bill_dom.js
@@ -1,22 +1,25 @@
-  var input = document.querySelector('.billTypeText');
-  var addBtn = document.querySelector('.addToBillBtn');
-  var callsTotal = document.querySelector('.callTotalOne');
-  var smsTotal = document.querySelector('.smsTotalOne');
-  var total = document.querySelector('.totalOne');
+var input = document.querySelector('.billTypeText');
+var addBtn = document.querySelector('.addToBillBtn');
+var callsTotal = document.querySelector('.callTotalOne');
+var smsTotal = document.querySelector('.smsTotalOne');
+var total = document.querySelector('.totalOne');
 
-  var textBillTotal = TextBillTotal();
+var textBillTotal = TextBillTotal();
 
   function textBillDom(){
+    
+    textBillTotal.calculation(input.value);
+    textBillTotal.totalCallSms(input.value);
 
       callsTotal.innerHTML = textBillTotal.returnCalls();
       smsTotal.innerHTML = textBillTotal.returnSms();
       total.innerHTML = textBillTotal.returnTotal();
 
-      if (total >= 50){
+      if (textBillTotal.returnTotal() >= 50){
 
           total.classList.add("danger");
       }
-      else if (total >= 30){
+      else if (textBillTotal.returnTotal() >= 30){
           total.classList.add("warning");
       }
 }
