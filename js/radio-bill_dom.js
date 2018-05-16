54232bf9423d147d87c766c90cb1ac38cf8effca
Candice <candice.m.sprout@gmail.com>
2018-05-16 10:24:41 +0200

    Radio merge complete

--- js/radio-bill_dom.js
+++ js/radio-bill_dom.js
@@ -3,18 +3,30 @@
   var smsTwo = document.querySelector('.smsTotalTwo');
   var totalBoth = document.querySelector('.totalTwo');
 
-  var checkedBtn = document.querySelector("input[name='billItemType']:checked");
+  var checked_Btn = document.querySelectorAll('.billItemTypeRadio');
+
+// var checked_Btn = document.querySelector("input[name='billItemType']:checked");
+//  if (checked_Btn) {
+//      var billItemType = checked_Btn.value
+//}
+
+  console.log(checked_Btn);
+  console.log(checked_Btn[0].value);
+  console.log(checked_Btn[1].value);
 
   var radioBill = RadioBill();
 
 function radioBillDom() {
+  var checkedBtn = '';
 
-  if (checkedBtn) {
-    var billItemType = checkedBtn.value;
+  for (var i = 0; i < checked_Btn.length; i++) {
+    if (checked_Btn[i].checked) {
+      checkedBtn = checked_Btn[i].value;
+    }
   }
-  
-  radioBill.calculateCallSms(billItemType);
-  radioBill.totalCalculate(billItemType);
+
+  radioBill.calculateCallSms(checkedBtn);
+  radioBill.totalCalculate();
 
     callsTwo.innerHTML = radioBill.callsTotal();
     smsTwo.innerHTML = radioBill.smsTotal();
